502f40536c1e5a5696df1aed75abf893aec8cb4a
useradmin <amutawa@hotmail.com>
2024-12-02 13:32:48 +0300

    end

--- clues.sql
+++ clues.sql
@@ -13,27 +13,32 @@ SELECT language FROM countrylanguages WHERE countrycode=(SELECT code FROM countr
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
+SELECT name FROM countries WHERE code IN (SELECT countrycode FROM countrylanguages WHERE language=(SELECT language FROM countrylanguages WHERE countrycode=(SELECT code FROM countries WHERE region='Southern Europe' ORDER BY population ASC LIMIT 1) AND isofficial=TRUE) GROUP BY countrycode HAVING COUNT(*)=1) AND region='Southern Europe';
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+SELECT name FROM cities WHERE countrycode=(SELECT code FROM countries WHERE name=(SELECT name FROM countries WHERE region='Southern Europe' ORDER BY population ASC LIMIT 1)) AND name!=(SELECT name FROM countries WHERE region='Southern Europe' ORDER BY population ASC LIMIT 1);
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
 
+SELECT name, countrycode FROM cities WHERE name LIKE '<اسم المدينة من Clue #4>%' AND name!='<اسم المدينة من Clue #4>' AND countrycode IN (SELECT code FROM countries WHERE region='South America');
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
+SELECT capital FROM countries WHERE code=(SELECT countrycode FROM cities WHERE name='<اسم المدينة من Clue #5>');
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
+SELECT name FROM cities WHERE population=91085;
 
 --               Our playdate of late has been unusually fun –
 --               As an agent, I'll say, you've been a joy to outrun.
